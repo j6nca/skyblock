222d82331a523a64dcadf94320971463c161f936
jonathan ng <me@j6n.ca>
2024-08-14 01:08:13 -0400

    add textures and config

---
+++ config.py
@@ -0,0 +1,9 @@
+worlds["skyblock"] = "./Infinite\ Skyblock"
+
+renders["normalrender"] = {
+    "world": "skyblock",
+    "title": "skyblock",
+}
+
+outputdir = "./static"
+texturepath = "./textures/vanilla"
