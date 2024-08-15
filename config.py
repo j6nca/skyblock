6047f0b4391e1dfe92fbd74ec282be463d84faa2
jonathan ng <me@j6n.ca>
2024-08-15 00:55:48 -0400

    update ignore file and config

--- config.py
+++ config.py
@@ -5,5 +5,19 @@ renders["normalrender"] = {
     "title": "skyblock",
 }
 
+renders["overworld"] = {
+    "world": "skyblock",
+    "title": "skyblock overworld",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+}
+
+renders["nether"] = {
+    "world": "skyblock",
+    "title": "skyblock nether",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether",
+}
+
 outputdir = "./static"
 texturepath = "./textures/vanilla"
